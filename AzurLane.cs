55efec1dd29e5fe683788b32bb8e540fd78977c7
anka <1350989414@qq.com>
2024-12-08 23:42:19 +0800

    我复活了!

--- AzurLane.cs
+++ AzurLane.cs
@@ -1,21 +1,15 @@
-using AzurLane.Contents.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Terraria.ModLoader;
 
 namespace AzurLane
 {
-    public class AzurLane : Mod
-    {
-        public static AzurLane Instance;
-        public ToggleUI ToggleUI;
+	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+	public class AzurLane : Mod
+	{
 
-        public AzurLane()
-        {
-            Instance = this;
-        }
-
-        public override void Load()
-        {
-            ToggleUI = new ToggleUI();
-        }
-    }
+	}
 }
